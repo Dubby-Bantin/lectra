ab032d23c687a1c787e06cdf315589c268feea4d
Dubby-Bantin <dubbybantin@gmail.com>
2024-11-13 22:00:26 +0100

    updated student auth

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,17 +1,16 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-// This middleware will run on every route under /admin
 export function middleware(request: NextRequest) {
   const userRole = request.cookies.get("role")?.value;
 
   if (userRole !== "admin") {
-    return NextResponse.redirect(new URL("/", request.url)); // Redirect to homepage if not an admin
+    return NextResponse.redirect(new URL("/", request.url));
   }
 
-  return NextResponse.next(); // Continue to admin page if user is an admin
+  return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/admin/:path*"], // Apply middleware to all admin routes
+  matcher: ["/admin/:path*"],
 };
