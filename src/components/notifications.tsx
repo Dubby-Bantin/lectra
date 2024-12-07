b32a9b1100a2431ab5ff7d01e45c0f55dc6a990b
Dubby Bantin <dubbybantin@icloud.com>
2024-12-07 18:12:34 +0100

    updated vercel env-varibles

--- src/components/notifications.tsx
+++ src/components/notifications.tsx
@@ -47,73 +47,77 @@ const Notifications = () => {
 
   return (
     studentId && (
-      <Popover>
-        <PopoverTrigger asChild>
-          <Button variant="outline" size="icon" className="relative p-2">
-            <Bell width={16} />
-            {hasNewNotifications && (
-              <div className="-top-0 -right-1 absolute flex justify-center items-center bg-blue-500 rounded-full w-4 h-4 text-white text-xs">
-                {notifications.length}
-              </div>
-            )}
-          </Button>
-        </PopoverTrigger>
-        <PopoverContent className="w-[400px]">
-          <div className="flex p-4">
-            <h3 className="font-medium text-lg">Notifications</h3>
-          </div>
-          <Separator />
-          <ScrollArea className="max-h-[400px]">
-            <div className="space-y-4 p-4">
-              {!hasNewNotifications && <p>You have no unread notifications.</p>}
-
-              {hasNewNotifications &&
-                notifications?.map(({ _id, text, roomId, _creationTime }) => (
-                  <div
-                    key={_id}
-                    className="items-start gap-4 grid grid-cols-[1fr_auto] w-full"
-                  >
-                    <div className="space-y-1 w-full">
-                      <p className="font-medium text-sm">{text}</p>
-                      <div className="flex justify-between items-center w-full">
-                        <p className="text-muted-foreground text-xs">
-                          {" "}
-                          {timeAgo(_creationTime)}
-                        </p>
-                        <Link href={`/video_lecture/${roomId}`}>
-                          <Button variant={"outline"} size={"sm"}>
-                            Join Lecture
-                          </Button>
-                        </Link>
-                      </div>
-                    </div>
-                    <Button
-                      onClick={() => markAsRead({ id: _id })}
-                      variant="ghost"
-                      size="icon"
-                    >
-                      <CheckIcon className="w-5 h-5" />
-                    </Button>
-                  </div>
-                ))}
+      <div className="flex justify-end p-5">
+        <Popover >
+          <PopoverTrigger asChild className="relative right-0">
+            <Button variant="outline" size="icon" className="relative p-2">
+              <Bell width={16} />
+              {hasNewNotifications && (
+                <div className="-top-0 -right-1 absolute flex justify-center items-center bg-blue-500 rounded-full w-4 h-4 text-white text-xs">
+                  {notifications.length}
+                </div>
+              )}
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent className="w-[400px]">
+            <div className="flex p-4">
+              <h3 className="font-medium text-lg">Notifications</h3>
             </div>
-          </ScrollArea>
-          {hasNewNotifications && (
-            <>
-              <Separator />
-              <div className="p-4">
-                <Button
-                  variant="outline"
-                  className="w-full"
-                  onClick={() => markAllAsRead({ userId: studentId })}
-                >
-                  Mark all as read
-                </Button>
+            <Separator />
+            <ScrollArea className="max-h-[400px]">
+              <div className="space-y-4 p-4">
+                {!hasNewNotifications && (
+                  <p>You have no unread notifications.</p>
+                )}
+
+                {hasNewNotifications &&
+                  notifications?.map(({ _id, text, roomId, _creationTime }) => (
+                    <div
+                      key={_id}
+                      className="items-start gap-4 grid grid-cols-[1fr_auto] w-full"
+                    >
+                      <div className="space-y-1 w-full">
+                        <p className="font-medium text-sm">{text}</p>
+                        <div className="flex justify-between items-center w-full">
+                          <p className="text-muted-foreground text-xs">
+                            {" "}
+                            {timeAgo(_creationTime)}
+                          </p>
+                          <Link href={`/video_lecture/${roomId}`}>
+                            <Button variant={"outline"} size={"sm"}>
+                              Join Lecture
+                            </Button>
+                          </Link>
+                        </div>
+                      </div>
+                      <Button
+                        onClick={() => markAsRead({ id: _id })}
+                        variant="ghost"
+                        size="icon"
+                      >
+                        <CheckIcon className="w-5 h-5" />
+                      </Button>
+                    </div>
+                  ))}
               </div>
-            </>
-          )}
-        </PopoverContent>
-      </Popover>
+            </ScrollArea>
+            {hasNewNotifications && (
+              <>
+                <Separator />
+                <div className="p-4">
+                  <Button
+                    variant="outline"
+                    className="w-full"
+                    onClick={() => markAllAsRead({ userId: studentId })}
+                  >
+                    Mark all as read
+                  </Button>
+                </div>
+              </>
+            )}
+          </PopoverContent>
+        </Popover>
+      </div>
     )
   );
 };
